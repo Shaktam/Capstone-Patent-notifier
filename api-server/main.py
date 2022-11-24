691475d2d1bbe4a35463f741cd3f5015085fe657
Shakti Thandavan <srishakti193@gmail.com>
2022-11-25 00:44:38 +0100

    add main.ppy

---
+++ api-server/main.py
@@ -0,0 +1,47 @@
+import requests
+import json
+api_url_base = 'https://search.patentsview.org/api/v1/patent/10757852/'
+api_token = 'n8eiYbx8.oyBat24OZjTDA40SZ09tKrTvTiqgImye'
+
+headers = {'content-type' : 'application/json',
+'X-CSRFToken': 'zPZwOFHnynLs69FHTvhPeb868YPutic9hnTCz9WwK2HyIF0MMDAYkIJ9sUVa6juG',
+    'X-Api-Key': 'n8eiYbx8.oyBat24OZjTDA40SZ09tKrTvTiqgImye'}
+auth_response = requests.get(api_url_base, headers=headers)
+response=json.loads(auth_response.content.decode('utf-8'))
+print(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import requests
+# r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
+# r.status_code
+# 200
+# r.headers['content-type']
+# 'application/json; charset=utf8'
+# r.encoding
+# 'utf-8'
+# r.text
+# '{"authenticated": true, ...'
+# r.json()
+# {'authenticated': True}
+
+
+# 'x-content-type-options': 'nosniff',
+# 'charset':'utf8' ,
