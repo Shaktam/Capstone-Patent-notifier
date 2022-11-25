ecb4fc629e6cfbdfe0844bb603d7a39f073349d3
Shakti <114337022+Shaktam@users.noreply.github.com>
2022-11-25 11:31:30 +0100

    Update main.py

--- api-server/main.py
+++ api-server/main.py
@@ -1,8 +1,6 @@
 import requests
 import json
 api_url_base = 'https://search.patentsview.org/api/v1/patent/?q={"_and":[{"_gte":{"patent_date":"2006-01-01"}}]}'
-api_token = 'n8eiYbx8.oyBat24OZjTDA40SZ09tKrTvTiqgImye'
-
 headers = {'content-type' : 'application/json'}
 auth_response = requests.get(api_url_base, headers=headers)
 response=json.loads(auth_response.content.decode('utf-8'))
