ccef2f9575b6d7f7f2a7c9e683258d1e3500e434
Shakti Thandavan <srishakti193@gmail.com>
2022-11-25 11:29:30 +0100

    edit main

--- api-server/main.py
+++ api-server/main.py
@@ -1,11 +1,9 @@
 import requests
 import json
-api_url_base = 'https://search.patentsview.org/api/v1/patent/10757852/'
+api_url_base = 'https://search.patentsview.org/api/v1/patent/?q={"_and":[{"_gte":{"patent_date":"2006-01-01"}}]}'
 api_token = 'n8eiYbx8.oyBat24OZjTDA40SZ09tKrTvTiqgImye'
 
-headers = {'content-type' : 'application/json',
-'X-CSRFToken': 'zPZwOFHnynLs69FHTvhPeb868YPutic9hnTCz9WwK2HyIF0MMDAYkIJ9sUVa6juG',
-    'X-Api-Key': 'n8eiYbx8.oyBat24OZjTDA40SZ09tKrTvTiqgImye'}
+headers = {'content-type' : 'application/json'}
 auth_response = requests.get(api_url_base, headers=headers)
 response=json.loads(auth_response.content.decode('utf-8'))
 print(response)
