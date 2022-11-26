35e97eed5b4fd829125be6f57df4c877a1436af6
Shakti Thandavan <srishakti193@gmail.com>
2022-11-26 22:22:35 +0100

    add loacal.iamrole in variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
     type = string
     default = "us-west-2"
 }
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  iam_role   = join(":", ["arn:aws:iam:", local.account_id, "role/LabRole"])
+}
