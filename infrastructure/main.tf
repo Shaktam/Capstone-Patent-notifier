3de714774fa82d1eeebcdd0d306d7df04720e554
Shakti Thandavan <srishakti193@gmail.com>
2022-11-27 19:48:54 +0100

    add backend s3 terraform in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,16 @@
 terraform {
-   required_providers {
-     aws = {
-       source  = "hashicorp/aws"
-       version = "~> 4.0"
-     }
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+   backend "s3" {
+    bucket = "terraform-state-dynodb-patent"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
 
  # Configure the AWS Provider
 provider "aws" {
