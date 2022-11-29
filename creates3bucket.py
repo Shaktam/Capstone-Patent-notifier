8d8778313d0dd6486fc0be26c1d8b7888da6229c
Shakti Thandavan <srishakti193@gmail.com>
2022-11-29 09:47:50 +0100

    edit bucket

--- creates3bucket.py
+++ creates3bucket.py
@@ -5,14 +5,17 @@ s3 = boto3.resource('s3')
 client = boto3.client('s3',region_name=AWS_REGION)
 location = {'LocationConstraint': AWS_REGION}
 statefiles3='terraform-state-dynodb-patent'
+dynamodb_bucket='patent-notifier-dynodb'
+patent_id_bucket='patent-json-id-bucket'
+
 
 def backend_tfbucket():
     if s3.Bucket(f'{statefiles3}') in s3.buckets.all():
-        print(f" {statefiles3} Exists!")
+        print(f" Bucket {statefiles3} Exists!")
         return True
     else:
         client.create_bucket(Bucket=statefiles3,CreateBucketConfiguration=location)
         print(f"Created a new bucket while bucket {statefiles3} does not exist!")
         return False
-
+        
 backend_tfbucket()
