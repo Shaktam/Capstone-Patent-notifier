534b24a6acf89ce77117a60bd415a04dec1dda12
Shakti <114337022+Shaktam@users.noreply.github.com>
2022-12-02 17:04:24 +0100

    Update lambda.tf

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "patent_lambda_crawler" {
-  filename     = "dbbuild/apis.zip"
+  filename     = "dbbuild/api-server.zip"
   function_name = "patent-lambda-crawler"
   role          = local.iam_role  
   handler       = "patent.handler"
   timeout       = 300
   runtime       = "python3.9"
-  source_code_hash = filebase64sha256("dbbuild/apis.zip")
+  source_code_hash = filebase64sha256("dbbuild/api-server.zip")
 
   environment {
     variables = {
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "patent_lambda_crawler" {
 }
 
 resource "aws_lambda_layer_version" "lambda_layer" {
-  filename   = "dbbuild/requests-layer.zip"
+  filename   = "dbbuild/requests_layer.zip"
   layer_name = "lambda_layer_name"
 
   compatible_runtimes = ["python3.9"]
