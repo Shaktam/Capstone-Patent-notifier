cd5e0e2e3770d08f5489e9b45fcd11b14fc3c585
Shakti Thandavan <srishakti193@gmail.com>
2022-12-02 14:59:40 +0100

    dynamodb update code

--- api-server/source/dynamodb_data.py
+++ api-server/source/dynamodb_data.py
@@ -1,14 +1,15 @@
 import boto3
-
+import os
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 
 # Create the DynamoDB table.
-patent_Table = dynamodb.Table('patent_dynamodb_table')
+patent_table_name = os.getenv('PATENTS_TABLE_NAME')
+patent_Table = dynamodb.Table(patent_table_name)
 
 def save_patent_to_db(data):
-    patent_Table.put_item(Item = data)
-
+    patent_Table.put_item(Item =data)
+    
 def save_patent_datas(patent_data):
     for data in patent_data:
         save_patent_to_db(data)
