dbdba5cc9e397114633094755f440a30fde5953d
Shakti Thandavan <srishakti193@gmail.com>
2022-12-03 00:12:50 +0100

    add apiserver and requestlayer

---
+++ upload_source.sh
@@ -0,0 +1,18 @@
+python3 creates3bucket.py
+
+mkdir infrastructure/dbbuild
+
+echo "zipfile for function"
+
+cd api-server/source
+zip -r ../../infrastructure/dbbuild/api-server.zip .
+cd ../..
+
+# cd notifier/src
+# zip -r ../../infrastructure/build/notifier.zip .
+# cd ../..
+
+cd requests_layer
+pip3 install -r requirements.txt --target python/lib/python3.9/site-packages
+zip -r ../infrastructure/dbbuild/requests_layer.zip .
+cd ..
