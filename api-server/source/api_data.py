a25c6f5eb03d1e92b4526553b5c13a5a84554512
Shakti Thandavan <srishakti193@gmail.com>
2022-12-03 00:10:22 +0100

    edit code

--- api-server/source/api_data.py
+++ api-server/source/api_data.py
@@ -1,26 +1,22 @@
-git checkout -q --track origin/api-datafrom main import get_patent_ids
-import requests
 import json
 import re
+import requests
 
-
-patent_id_list= get_patent_ids()
-store_patent_id=[]
 def get_id(patent_id_list):
+    store_patent_id=[]
     for patent_id in patent_id_list:
-        store_patent_id.append(patent_id['patent_id'])
+        store_patent_id.append(patent_id['patent_id'])  
     return store_patent_id
-    
-stored_patent_id=get_id(patent_id_list)
-patent_data=[]
+
 def get_patent_datas(stored_patent_id):
+    patent_data=[]  
     baseurl='https://api.patentsview.org/patents/query?q={"patent_id":'
     field = '}&f=["patent_id","patent_title","patent_abstract","assignee_organization","patent_date"]'
     for id in stored_patent_id:
         url= baseurl +str(id) + field
-        r = requests.get(url)
-        json_patent_data = json.loads(r.content.decode('utf-8'))
-        for patent in json_patent_data['patents']:
+        response = requests.get(url)
+        obj_data = json.loads(response.content.decode('utf-8'))
+        for patent in obj_data['patents']:
             get_organisation_data = "" if 'assignee_organization'== None else [assignee['assignee_organization']for assignee in patent['assignees']]
             organisation_string=json.dumps(get_organisation_data)
             remove_bracket_from_string=re.sub(r'[\[\]]', r'', organisation_string)
@@ -32,7 +28,5 @@ def get_patent_datas(stored_patent_id):
                 "patent_date":patent['patent_date'],
                 "organization":organization
                 }
-        patent_data.append(list_data)     
+        patent_data.append(list_data)      
     return patent_data
-
-    
--- api-server/source/lambda_function.py
+++ api-server/source/lambda_function.py
@@ -1,13 +1,12 @@
-from main import get_patent_ids
-from api_data import store_only_ids,get_patent_datas
 from dynamodb_data import save_patent_datas
+from api_data import get_patent_datas,get_id
+from main import get_patent_ids
 
 def lambda_handler(event,context):
-    patent_id= get_patent_ids()
-    store_patent_id=store_only_ids(patent_id)
-    patent_data=get_patent_datas(store_patent_id)
+    patent_id_list=get_patent_ids()
+    stored_patent_id=get_id(patent_id_list)
+    patent_data=get_patent_datas(stored_patent_id)
     return save_patent_datas(patent_data)
 
 if __name__ == "__main__":
     lambda_handler({},{})
-      
