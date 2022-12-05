c4fbfdb47068197e63826e955246fe683f51e785
Shakti <114337022+Shaktam@users.noreply.github.com>
2022-12-05 12:59:11 +0100

    Update main.py

--- api-server/source/main.py
+++ api-server/source/main.py
@@ -7,9 +7,10 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 import os
 
-api_url_base = 'https://search.patentsview.org/api/v1/patent/?q={"_and":[{"_gt":{"patent_year":"2020" }},{"_lte":{"patent_date":"2023"}}]}&f=["patent_id"]'
-PATENT_CSRF_TOKEN= os.getenv("PATENT_CSRF_TOKEN")
-PATENT_API_KEY =  os.getenv("PATENT_API_KEY")
+for year in range(2020,2023):
+    api_url_base = 'https://search.patentsview.org/api/v1/patent/?q={"patent_year":'+ str(year)+'}&f=["patent_id"]&o={"size":1000}'
+    PATENT_CSRF_TOKEN= os.getenv("PATENT_CSRF_TOKEN")
+    PATENT_API_KEY =  os.getenv("PATENT_API_KEY")
 
 def get_patent_ids():
     headers = {'content-type' : 'application/json',
