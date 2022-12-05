94468063a35b37be76bb138e2a14c7654afb3788
Shakti <114337022+Shaktam@users.noreply.github.com>
2022-12-05 11:32:03 +0100

    Update api_data.py

--- api-server/source/api_data.py
+++ api-server/source/api_data.py
@@ -2,23 +2,25 @@ import json
 import re
 import requests
 
-baseurl='https://api.patentsview.org/patents/query?q={"patent_id":'
-field = '}&f=["patent_id","patent_title","patent_abstract","assignee_organization","patent_date"]'
-
 def get_id(patent_id_list):
     store_patent_id=[]
     for patent_id in patent_id_list:
         store_patent_id.append(patent_id['patent_id'])  
     return store_patent_id
 
+def get_patent(id):
+    baseurl='https://api.patentsview.org/patents/query?q={"patent_id":'
+    field = '}&f=["patent_id","patent_title","patent_abstract","assignee_organization","patent_date"]'
+    url= baseurl +str(id) + field
+    response = requests.get(url)
+    return json.loads(response.content.decode('utf-8'))
+
 def get_patent_datas(stored_patent_id):
     patent_data=[]  
     for id in stored_patent_id:
-        url= baseurl +str(id) + field
-        response = requests.get(url)
-        obj_data = json.loads(response.content.decode('utf-8'))
+        obj_data =get_patent(id)
         for patent in obj_data['patents']:
-            get_organisation_data = "" if 'assignee_organization'== None else [assignee['assignee_organization'] for assignee in patent['assignees']]
+            get_organisation_data = "" if 'assignee_organization'== None else [assignee['assignee_organization']for assignee in patent['assignees']]
             organisation_string=json.dumps(get_organisation_data)
             remove_bracket_from_string=re.sub(r'[\[\]]', r'', organisation_string)
             organization = remove_bracket_from_string.replace('"','')
@@ -30,4 +32,6 @@ def get_patent_datas(stored_patent_id):
                 "organization":organization
                 }
         patent_data.append(list_data)      
+        print(patent_data)    
     return patent_data
+
