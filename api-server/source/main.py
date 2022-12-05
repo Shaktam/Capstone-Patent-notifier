2c24e178d8711a01b68ec9550339c02e54dcc522
Shakti <114337022+Shaktam@users.noreply.github.com>
2022-12-05 14:05:23 +0100

    Update main.py

--- api-server/source/main.py
+++ api-server/source/main.py
@@ -7,15 +7,15 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 import os
 
-api_url_base = 'https://search.patentsview.org/api/v1/patent/?q={"patent_year":"2020"}&f=["patent_id"]'
-PATENT_CSRF_TOKEN= os.getenv("PATENT_CSRF_TOKEN")
-PATENT_API_KEY =  os.getenv("PATENT_API_KEY")
+for year in range(2020,2023):
+    api_url_base = 'https://search.patentsview.org/api/v1/patent/?q={"patent_year":'+ str(year)+'}&f=["patent_id"]&o={"size":1000}'
+    PATENT_CSRF_TOKEN= os.getenv("PATENT_CSRF_TOKEN")
+    PATENT_API_KEY =  os.getenv("PATENT_API_KEY")
 
 def get_patent_ids():
     headers = {'content-type' : 'application/json',
                'X-CSRFToken': PATENT_CSRF_TOKEN,
                'X-Api-Key': PATENT_API_KEY}
     auth_response = requests.get(api_url_base, headers=headers)
-    patent_id=json.loads(auth_response.content.decode('utf-8'))["patents"]
-    return patent_id
-    
+    patent_id_list=json.loads(auth_response.content.decode('utf-8'))["patents"] 
+    return patent_id_list
