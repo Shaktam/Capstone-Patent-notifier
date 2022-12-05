346cd7b9b33bff625362075a8c6783f491efee5b
Shakti <114337022+Shaktam@users.noreply.github.com>
2022-12-05 12:45:13 +0100

    Update api_data.py

--- api-server/source/api_data.py
+++ api-server/source/api_data.py
@@ -15,15 +15,18 @@ def get_patent(id):
     response = requests.get(url)
     return json.loads(response.content.decode('utf-8'))
 
+def get_organization(patent):
+    get_organisation_data = "" if 'assignee_organization'== None else [assignee['assignee_organization']for assignee in patent['assignees']]
+    organisation_string=json.dumps(get_organisation_data)
+    remove_bracket_from_string=re.sub(r'[\[\]]', r'', organisation_string)
+    return remove_bracket_from_string.replace('"','')
+
 def get_patent_datas(stored_patent_id):
     patent_data=[]  
     for id in stored_patent_id:
-        obj_data =get_patent(id)
+        obj_data=get_patent(id)
         for patent in obj_data['patents']:
-            get_organisation_data = "" if 'assignee_organization'== None else [assignee['assignee_organization']for assignee in patent['assignees']]
-            organisation_string=json.dumps(get_organisation_data)
-            remove_bracket_from_string=re.sub(r'[\[\]]', r'', organisation_string)
-            organization = remove_bracket_from_string.replace('"','')
+            organization=get_organization(patent)
             list_data ={
                 "patent_id":patent['patent_id'],
                 "title":patent['patent_title'],
@@ -34,4 +37,3 @@ def get_patent_datas(stored_patent_id):
         patent_data.append(list_data)      
         print(patent_data)    
     return patent_data
-
