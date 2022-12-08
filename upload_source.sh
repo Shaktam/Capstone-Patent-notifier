e09a5ff733e02641facec6077c314e465389777a
Shakti Thandavan <srishakti193@gmail.com>
2022-12-08 21:59:18 +0100

    upload_source.sh

--- upload_source.sh
+++ upload_source.sh
@@ -1,24 +1,21 @@
+echo "Creating a Bucket for Terraform State"
 python3 creates3bucket.py
+
+echo "Creating a Bucket for Patent Data"
 sh patent_s3_bucket.sh
 
 mkdir infrastructure/dbbuild
 
 echo "zipfile for function"
 
-cd api-server/source
-zip -r ../../infrastructure/dbbuild/api-server.zip .
+cd patent-data-function/source
+zip -r ../../infrastructure/dbbuild/patent-data-function.zip .
 cd ../..
 
 cd requests_layer
 pip3 install -r requirements.txt --target python/lib/python3.9/site-packages
 zip -r ../infrastructure/dbbuild/requests_layer.zip .
 cd ..
-
-echo "Zip files for notifier"
-
-cd patent_notifier/source
-zip -r ../../infrastructure/dbbuild/notifier.zip .
-cd ../..
 
 mkdir build
 
