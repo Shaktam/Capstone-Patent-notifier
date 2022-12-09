3ca626af379c20eba780a04598cbe5764d5eee74
Shakti <114337022+Shaktam@users.noreply.github.com>
2022-12-09 19:10:21 +0100

    Update lambda_invoke.sh

--- lambda_invoke.sh
+++ lambda_invoke.sh
@@ -2,19 +2,19 @@ PATENT_FUNCTION="Patent Lambda Crawler Function"
 NOTIFIER_FUNCTION="Patent Notifier"
 
 echo "Adding Permission for $PATENT_FUNCTION URL"
-aws lambda add-permission --function-name patent-lambda-crawler --action lambda:InvokeFunctionUrl --principal "*" --function-url-auth-type "AWS_IAM" --statement-id url
+aws lambda add-permission --function-name patent-lambda-crawler --action lambda:InvokeFunctionUrl --principal "*" --function-url-auth-type "NONE" --statement-id url
 
 echo "Creating $PATENT_FUNCTION  URL"
-aws lambda create-function-url-config --function-name patent-lambda-crawler --auth-type AWS_IAM
+aws lambda create-function-url-config --function-name patent-lambda-crawler --auth-type NONE
 
 echo "Configuring test-event for $PATENT_FUNCTION and running the code"
 aws lambda invoke --function-name patent-lambda-crawler --region us-west-2 --cli-binary-format raw-in-base64-out --cli-read-timeout 900 --payload file://dynamodb_payload.json response.json
 
 echo"Adding Permission for $NOTIFIER_FUNCTION URL"
-aws lambda add-permission --function-name notifer --action lambda:InvokeFunctionUrl --principal "*" --function-url-auth-type "AWS_IAM" --statement-id url
+aws lambda add-permission --function-name notifer --action lambda:InvokeFunctionUrl --principal "*" --function-url-auth-type "NONE" --statement-id url
 
 echo "Creating $NOTIFIER_FUNCTION URL"
-aws lambda create-function-url-config --function-name notifer --auth-type AWS_IAM
+aws lambda create-function-url-config --function-name notifer --auth-type NONE
 
 echo "Configuring test-event for $NOTIFIER_FUNCTION and running the code"
 aws lambda invoke --function-name notifer --region us-west-2 --cli-binary-format raw-in-base64-out --cli-read-timeout 600 --payload file://sns_payload.json response.json
