11b7f4ac063fde2117567f8130cccfb95f75474d
Shakti Thandavan <srishakti193@gmail.com>
2022-12-09 18:54:10 +0100

    changed file name

---
+++ patent-data-function/source/api_data.py
@@ -0,0 +1,38 @@
+import json
+import re
+import requests
+
+def get_id(patent_id_list):
+    store_patent_id=[]
+    for patent_id in patent_id_list:
+        store_patent_id.append(patent_id['patent_id'])  
+    return store_patent_id
+
+def get_patent(id):
+    baseurl='https://api.patentsview.org/patents/query?q={"patent_id":'
+    field = '}&f=["patent_id","patent_title","patent_abstract","assignee_organization","patent_date"]'
+    url= baseurl +str(id) + field
+    response = requests.get(url)
+    return json.loads(response.content.decode('utf-8'))
+
+def get_organization(patent):
+    get_organisation_data = "" if 'assignee_organization'== None else [assignee['assignee_organization']for assignee in patent['assignees']]
+    organisation_string=json.dumps(get_organisation_data)
+    remove_bracket_from_string=re.sub(r'[\[\]]', r'', organisation_string)
+    return remove_bracket_from_string.replace('"','')
+
+def get_patent_datas(stored_patent_id):
+    patent_data=[]  
+    for id in stored_patent_id:
+        obj_data=get_patent(id)
+        for patent in obj_data['patents']:
+            organization=get_organization(patent)
+            list_data ={
+                "patent_id":patent['patent_id'],
+                "title":patent['patent_title'],
+                "abstract":patent['patent_abstract'],
+                "patent_date":patent['patent_date'],
+                "organization":organization
+                }
+        patent_data.append(list_data)        
+    return patent_data
---
+++ patent-data-function/source/main.py
@@ -0,0 +1,21 @@
+import requests
+import logging
+import json
+import os
+from dotenv import load_dotenv
+from pathlib import Path  # python3 only
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+for year in range(2020,2023):
+    api_url_base = 'https://search.patentsview.org/api/v1/patent/?q={"patent_year":'+ str(year)+'}&f=["patent_id"]&o={"size":1000}'
+    PATENT_CSRF_TOKEN= os.getenv("PATENT_CSRF_TOKEN")
+    PATENT_API_KEY =  os.getenv("PATENT_API_KEY")
+
+def get_patent_ids():
+    headers = {'content-type' : 'application/json',
+               'X-CSRFToken': PATENT_CSRF_TOKEN,
+               'X-Api-Key': PATENT_API_KEY}
+    auth_response = requests.get(api_url_base, headers=headers)
+    patent_id_list=json.loads(auth_response.content.decode('utf-8'))["patents"] 
+    return patent_id_list
