9f615d191379cb5685c4d35e48c09aaf7af2d75f
Shakti Thandavan <srishakti193@gmail.com>
2022-12-20 00:00:56 +0100

    add file for s3 and dynamodb

--- lambda_invoke.sh
+++ lambda_invoke.sh
@@ -1,5 +1,15 @@
 PATENT_FUNCTION="Patent Lambda Crawler Function"
 NOTIFIER_FUNCTION="Patent Notifier"
+S3DYNAMODB_FUNCTION="Function to s3 and dynamodb"
+
+echo"Adding Permission for $S3DYNAMODB_FUNCTION URL"
+aws lambda add-permission --function-name patent-s3-dynamodb --action lambda:InvokeFunctionUrl --principal "*" --function-url-auth-type "NONE" --statement-id url
+
+echo "Creating $S3DYNAMODB_FUNCTION URL"
+aws lambda create-function-url-config --function-name patent-s3-dynamodb --auth-type NONE
+
+echo "Configuring test-event for $S3DYNAMODB_FUNCTION and running the code"
+aws lambda invoke --function-name patent-s3-dynamodb --region us-west-2 --cli-binary-format raw-in-base64-out --cli-read-timeout 900 --payload file://s3_payload.json response.json
 
 echo "Adding Permission for $PATENT_FUNCTION URL"
 aws lambda add-permission --function-name patent-lambda-crawler --action lambda:InvokeFunctionUrl --principal "*" --function-url-auth-type "NONE" --statement-id url
