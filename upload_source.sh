99dcf2370a1b93cf29f5da5cdf3b1ccc20def523
Shakti Thandavan <srishakti193@gmail.com>
2022-12-20 00:08:16 +0100

    update source file

--- upload_source.sh
+++ upload_source.sh
@@ -2,7 +2,10 @@ echo "Creating a Bucket for Terraform State"
 python3 creates3bucket.py
 
 echo "Creating a Bucket for Patent Data"
-sh patent_s3_bucket.sh
+sh patent_s3_bucket.sh patent-data-informer
+
+echo "Creating a Bucket for Patent Data to dynamodb"
+sh patent_s3_bucket.sh patent-datas-to-dynamodb
 
 mkdir infrastructure/dbbuild
 
@@ -23,6 +26,12 @@ cd patent_notifier/source
 zip -r ../../infrastructure/dbbuild/notifier.zip .
 cd ../..
 
+echo "Zipfile for patent data from s3 to dynamodb"
+
+cd s3_patent_function
+zip -r ../infrastructure/dbbuild/s3_patent_function.zip .
+cd ..
+
 mkdir build
 
 echo "zip files for server"
@@ -32,6 +41,8 @@ zip ../build/patent-data-server.zip requirements.txt
 zip -r ../build/patent-data-server.zip source 
 cd ..
 
-echo  "upload to s3"
+echo  "upload to s3 to access Dynamodb"
 aws s3 cp build/patent-data-server.zip s3://patent-data-informer/
 
+echo  "upload to s3 to send data to s3"
+aws s3 cp patent_datas.csv s3://patent-datas-to-dynamodb/
